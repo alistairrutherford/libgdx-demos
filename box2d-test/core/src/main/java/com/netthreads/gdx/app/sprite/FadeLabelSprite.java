8deb0caeb09b6357af2926e1a7f73641c63eb370
alistairrutherford <alistair.rutherford@gmail.com>
2013-03-12 17:05:09 +0000

    Updated comments.

--- box2d-test/core/src/main/java/com/netthreads/gdx/app/sprite/FadeLabelSprite.java
+++ box2d-test/core/src/main/java/com/netthreads/gdx/app/sprite/FadeLabelSprite.java
@@ -36,16 +36,16 @@ public class FadeLabelSprite extends Actor implements ActionCallBack
 {
 	private static final String FONT_FILE_SMALL = "data/font.fnt";
 	private static final String FONT_IMAGE_SMALL = "data/font.png";
-
+	
 	private BitmapFont touchMessage;
-
+	
 	private static final float DEFAULT_FADE_TIME = 1.0f;
 	private static float START_ALPHA = 1;
 	private static float END_ALPHA = 0;
 	private float fadeTime;
-
+	
 	private StringBuilder text = new StringBuilder(100);
-
+	
 	/**
 	 * Create layer which displays the FPS in the bottom corner.
 	 * 
@@ -55,10 +55,10 @@ public class FadeLabelSprite extends Actor implements ActionCallBack
 	public FadeLabelSprite()
 	{
 		fadeTime = DEFAULT_FADE_TIME;
-
+		
 		buildElements();
 	}
-
+	
 	/**
 	 * Build view elements.
 	 * 
@@ -67,7 +67,7 @@ public class FadeLabelSprite extends Actor implements ActionCallBack
 	{
 		touchMessage = new BitmapFont(Gdx.files.internal(FONT_FILE_SMALL), Gdx.files.internal(FONT_IMAGE_SMALL), false);
 	}
-
+	
 	/**
 	 * Run fade action.
 	 * 
@@ -76,14 +76,18 @@ public class FadeLabelSprite extends Actor implements ActionCallBack
 	{
 		// Initialise alpha.
 		getColor().a = START_ALPHA;
-
+		
 		AlphaAction fadeOut = Actions.alpha(END_ALPHA, fadeTime);
-
+		
+		// We will use a callback action to remove our label from the view. As
+		// noted in removeActor we can't clear out the associated actions
+		// because we are calling from within an action. Not sure that the
+		// solution to that is.
 		CallBackAction callBackAction = CallBackAction.$(this);
-
+		
 		addAction(Actions.sequence(fadeOut, callBackAction));
 	}
-
+	
 	/**
 	 * Draw touch message.
 	 * 
@@ -94,12 +98,12 @@ public class FadeLabelSprite extends Actor implements ActionCallBack
 		// Fade action is running against the Actor colour so assign it to the
 		// text.
 		touchMessage.setColor(getColor());
-
+		
 		touchMessage.setScale(getScaleX(), getScaleY());
-
+		
 		touchMessage.draw(batch, text, getX(), getY());
 	}
-
+	
 	/**
 	 * Assign text.
 	 * 
@@ -110,7 +114,7 @@ public class FadeLabelSprite extends Actor implements ActionCallBack
 		this.text.setLength(0);
 		this.text.append(text);
 	}
-
+	
 	/**
 	 * On callback remove the actor.
 	 * 
@@ -120,5 +124,5 @@ public class FadeLabelSprite extends Actor implements ActionCallBack
 	{
 		getParent().removeActor(this);
 	}
-
+	
 }
