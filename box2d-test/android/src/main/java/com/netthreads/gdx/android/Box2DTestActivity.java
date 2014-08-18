557b5af37b80dda04d763d7585f44370d6e9e749
alistairrutherford <alistair.rutherford@gmail.com>
2014-08-18 22:31:25 +0100

    Fixed pom and activity for Android.

--- box2d-test/android/src/main/java/com/netthreads/gdx/android/Box2DTestActivity.java
+++ box2d-test/android/src/main/java/com/netthreads/gdx/android/Box2DTestActivity.java
@@ -1,19 +1,25 @@
 package com.netthreads.gdx.android;
 
-import com.netthreads.gdx.core.Box2DTest;
-
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.netthreads.gdx.app.Box2DTest;
 
-public class Box2DTestActivity extends AndroidApplication {
-	
+/**
+ * Android Application Activity.
+ *
+ */
+public class Box2DTestActivity extends AndroidApplication
+{
+
 	@Override
-   public void onCreate(Bundle savedInstanceState) {
-       super.onCreate(savedInstanceState);
-       AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-       config.useGL20 = true;
-       initialize(new Box2DTest(), config);
-   }
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		
+		initialize(new Box2DTest(), config);
+	}
 }
